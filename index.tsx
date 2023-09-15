5a7bd48c99a3ff0036585afcb545d46997f1a77f
ikjunchoi <choiikjun1101@gmail.com>
2023-09-16 02:42:20 +0900

    functional coponent -> poor man's React

--- index.tsx
+++ index.tsx
@@ -1,10 +1,26 @@
 const React = {
-  createElement: (...args) => {
-    console.log(args);
+  createElement: (tag, props, ...children) => {
+    if (typeof tag === "function") {
+      try {
+        return tag(props);
+      } catch (error) {
+        console.error(error);
+        return null;
+      }
+    }
+    var element = { tag, props: { ...props, children } };
+    console.log(element);
+    return element;
   },
 };
-const a = (
-  <div className="good" id="nice">
-    hello
+const App = () => (
+  <div className="good">
+    <h1>hello</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias vero in neque adipisci quam laudantium iste, suscipit harum veritatis aspernatur odio reprehenderit, facilis vel ad voluptate
+      recusandae, doloremque repudiandae libero.
+    </p>
   </div>
 );
+
+<App />;
